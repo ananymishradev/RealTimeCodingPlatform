90987fb551cd8a70bdbb8823b88ee29c72c94592
Anany Mishra <anany.mishra.real@gmail.com>
2025-12-10 18:59:19 +0530

    Editor Update fix

---
+++ data/work/a1d48901-5a16-486a-ad08-daf39e40674d/main.js
@@ -0,0 +1,8 @@
+function solution() {
+    // Write your code here
+    console.log("Anany");
+    
+}
+
+// Test your solution
+console.log(solution());
---
+++ data/work/d16806e0-b928-4388-9d5b-7bd8fc64cce7/main.js
@@ -0,0 +1,7 @@
+function solution() {
+    // Write your code here
+    console.log("Anany");
+}
+
+// Test your solution
+console.log(solution());
